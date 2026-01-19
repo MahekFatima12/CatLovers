1b4deffce59fefa99ec5293134edcb9e1484df02
mahekfatima <mahekfatima191@gmail.com>
2026-01-19 05:07:08 -0500

    Add Careers, Contact, and WhatsApp buttons with UI improvements

---
+++ CatLoversWeb/Controllers/CareersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatLoversWeb.Controllers
+{
+    public class CareersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
